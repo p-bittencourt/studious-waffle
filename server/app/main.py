5d684e636176c9744cdfed753d49598618b3dc02
pedro <pedromonteirobittencourt@gmail.com>
2025-04-16 21:38:47 -0300

    feat: added boilerplate logging and exceptions

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,23 @@
+"""
+Main application module for the E-Commerce API
+This module initializes the FastAPI application, sets up routers,
+and defines the root endpoints.
+"""
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from .core.utils.logging import configure_logging, LogLevels
+
+configure_logging(LogLevels.INFO)
+
+app = FastAPI(
+    title="E-Commerce API",
+    description="Exercise done for Roadmap.sh Python roadmap",
+    version="0.1.0",
+)
 
 
 @app.get("/")
 async def root():
+    """Root endpoint for the API"""
     return {"message": "Hello World"}
