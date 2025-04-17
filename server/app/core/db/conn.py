417e9a1451edac54ec539be62f3b1c895d74e942
pedro <pedromonteirobittencourt@gmail.com>
2025-04-17 10:32:28 -0300

    feat: create_db_and_tables function on conn.py

--- server/app/core/db/conn.py
+++ server/app/core/db/conn.py
@@ -19,7 +19,10 @@ DB_NAME = os.getenv("DB_NAME")
 DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@" f"{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DB_URL)
-SQLModel.metadata.create_all(engine)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
 def get_session():
