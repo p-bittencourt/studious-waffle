ac8a347ddf5f8e1b5d49113c374d0f475c4d2969
pedro <pedromonteirobittencourt@gmail.com>
2025-04-26 16:44:43 -0300

    feat: checks a user password with bcrypt

--- server/app/core/auth/login.py
+++ server/app/core/auth/login.py
@@ -1,4 +1,5 @@
 import logging
+import bcrypt
 from sqlmodel import Session, select
 from app.core.db.user import Shopper, UserLoginFields
 from app.core.utils.exceptions import NotFound
@@ -10,11 +11,22 @@ def check_shopper_credentials(db: Session, login_data: UserLoginFields):
     shopper = get_shopper_by_email(db, login_data.email)
     logger.debug(f"Retrieved shopper by email {shopper}")
 
-    # password_check = check_password()
+    password_check = check_password(
+        "shopper", login_data.password, shopper.password_hash
+    )
     # if password_check:
     # return generate_access_token()
 
     return {"status": "failure", "message": "Couldn't login user"}
+
+
+def check_password(submitted_password: str, hashed_password: str) -> bool:
+    if bcrypt.checkpw(
+        submitted_password.encode("utf-8"), hashed_password.encode("utf-8")
+    ):
+        return True
+    else:
+        return False
 
 
 def get_shopper_by_email(db: Session, user_email: str):
