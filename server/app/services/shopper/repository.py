e9d73088799d58f3202fd7c0c1f832eefe8b5a85
pedro <pedromonteirobittencourt@gmail.com>
2025-05-02 15:08:15 -0300

    chore: removed classmethod decorator

--- server/app/services/shopper/repository.py
+++ server/app/services/shopper/repository.py
@@ -11,32 +11,25 @@ logger = logging.getLogger(__name__)
 class ShopperRepository:
     """Repository for Shopper database operations"""
 
-    @classmethod
-    def get_shoppers(cls, db: Session) -> List[Shopper]:
+    def get_shoppers(db: Session) -> List[Shopper]:
         """Retrieves all shoppers from the db"""
         return db.scalars(select(Shopper)).all()
 
-    @classmethod
-    def get_shopper_id(cls, db: Session, shopper_id: str) -> Shopper:
+    def get_shopper_id(db: Session, shopper_id: str) -> Shopper:
         """Retrieves a shopper by ID"""
         return db.scalar(select(Shopper).where(Shopper.id == shopper_id))
 
-    @classmethod
-    def get_shopper_email(cls, db: Session, shopper_email: str) -> Shopper:
+    def get_shopper_email(db: Session, shopper_email: str) -> Shopper:
         """Retrieves a shopper by email"""
         return db.scalar(select(Shopper).where(Shopper.email == shopper_email))
 
-    @classmethod
-    def get_shopper_name(cls, db: Session, shopper_name: str) -> Shopper:
+    def get_shopper_name(db: Session, shopper_name: str) -> Shopper:
         """Retrieves a shopper by name"""
         return db.scalar(select(Shopper).where(Shopper.email == shopper_name))
 
-    @classmethod
-    def update_shopper(
-        cls, db: Session, shopper_id: str, data: ShopperUpdate
-    ) -> Shopper:
+    def update_shopper(db: Session, shopper_id: str, data: ShopperUpdate) -> Shopper:
         """Updates user data"""
-        shopper = cls.get_shopper_id(db, shopper_id)
+        shopper = ShopperRepository.get_shopper_id(db, shopper_id)
         if not shopper:
             logger.warning("User with id %s was not found")
             raise NotFound(detail="User not found")
