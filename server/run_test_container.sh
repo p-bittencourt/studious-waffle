6dc007f8f1890129fdff89451a1586d00c949f77
pedro <pedromonteirobittencourt@gmail.com>
2025-05-05 11:23:08 -0300

    chore: removed hyphen on docker compose

--- server/run_test_container.sh
+++ server/run_test_container.sh
@@ -5,14 +5,14 @@ PROJECT_NAME="studious_waffle_test"
 
 # Make sure any old test containers are cleaned up first
 echo "Cleaning up any previous test environment..."
-docker-compose -f docker-compose.test.yml -p $PROJECT_NAME down --remove-orphans
+docker compose -f docker-compose.test.yml -p $PROJECT_NAME down --remove-orphans
 
 echo "Starting test containers..."
 # Run the tests with the isolated project name
-docker-compose -f docker-compose.test.yml -p $PROJECT_NAME up --build --exit-code-from test
+docker compose -f docker-compose.test.yml -p $PROJECT_NAME up --build --exit-code-from test
 
 echo "Tests completed. Cleaning up test environment..."
 # Clean up everything related to this test run
-docker-compose -f docker-compose.test.yml -p $PROJECT_NAME down --remove-orphans
+docker compose -f docker-compose.test.yml -p $PROJECT_NAME down --remove-orphans
 
 echo "Test environment cleanup complete."
